37d53816b406a5d2e6719b512028f89b4c264aad
clever_WangErgou <48337433+dmu-wxy@users.noreply.github.com>
2021-04-08 11:50:08 +0800

    fix blog action

--- deploy.sh
+++ deploy.sh
@@ -25,7 +25,7 @@ echo 'www.smartdog.top' > CNAME  # 自定义域名
 if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
   codingUrl=https://e.coding.net/meteor7/blog/blog.git
 else
-  codingUrl=https://e.coding.net/meteor7/blog/blog.git # https://xugaoyi:${CODING_TOKEN}@git.dev.tencent.com/xugaoyi/xugaoyi.git
+  codingUrl=https://meteor7:${CODING_TOKEN}@git.dev.tencent.com/blog/blog.git
 fi
 git add -A
 git commit -m "${msg}"
